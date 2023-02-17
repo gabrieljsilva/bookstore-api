d1de89e88a0fe7c5610c911c21d0f4a0db354b7d
Gabriel Silva <gabrieljsilva2301@gmail.com>
2023-02-16 22:17:18 -0300

    added: who am I endpoint

--- src/packages/user/modules/user/data-views/user.data-view.ts
+++ src/packages/user/modules/user/data-views/user.data-view.ts
@@ -1,21 +1,31 @@
-import { User, Credentials } from '@prisma/client';
+import { User, Credentials, Role } from '@prisma/client';
 
-type UserDatabaseModel = User & { credentials: Credentials };
+type UserDatabaseModel = User & {
+  credentials: Credentials & { roles?: Role[] };
+};
 
 export class UserDataView {
   id: string;
   name: string;
   email: string;
+  roles?: string[];
 
   constructor(user: UserDataView) {
     this.id = user.id;
     this.name = user.name;
     this.email = user.email;
+    this.roles = user.roles;
   }
 
   static fromDatabaseModel(user: UserDatabaseModel): UserDataView {
     const { id, name, credentials } = user;
-    const { email } = credentials;
-    return new UserDataView({ id, name, email });
+    const { email, roles } = credentials;
+
+    return new UserDataView({
+      id,
+      name,
+      email,
+      roles: roles && roles.map((role) => role.name),
+    });
   }
 }
--- src/packages/user/modules/user/user.controller.ts
+++ src/packages/user/modules/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos';
 import { UserDataView } from './data-views';
-import { IsPublic } from '@decorators';
+import { CurrentUser, IsPublic } from '@decorators';
+import { UserModel } from '@models';
 
 @Controller('/users')
 export class UserController {
@@ -12,6 +13,11 @@ export class UserController {
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
     const user = await this.userService.createUser(createUserDto);
+    return UserDataView.fromDatabaseModel(user);
+  }
+
+  @Get('who-am-i')
+  async whoAmI(@CurrentUser() user: UserModel) {
     return UserDataView.fromDatabaseModel(user);
   }
 }
